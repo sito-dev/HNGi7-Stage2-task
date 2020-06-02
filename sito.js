7b6a11cef7ae41af1cb28cf357afbaae74773995
sito-hub <66259019+sito-hub@users.noreply.github.com>
2020-06-02 21:04:20 +0100

    Update and rename README.md to sito.js

---
+++ sito.js
@@ -0,0 +1,15 @@
+function sitoInformation(){
+//my information Object goes here.
+const personalInformation = {
+"fullName": "Ogundare Israel Taiwo",
+"ID": "HNG-03186",
+"codeLanguage": "Javascript"
+}
+
+//printing my information
+document.write(`Hello world, this is ${personalInformation.fullName} with HNGi7 ID: ${personalInformation.ID} using ${personalInformation.codeLanguage} for Stage 2 Task`);
+
+}
+
+//calling my sitoInformation function
+sitoInformation();
