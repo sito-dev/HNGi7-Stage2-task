b18f89e5cb0434a1ad554b94ba484500cc2ff504
sito-hub <66259019+sito-hub@users.noreply.github.com>
2020-06-03 01:26:30 +0100

    Update sito.js

--- sito.js
+++ sito.js
@@ -1,15 +1,29 @@
-function sitoInformation(){
+<script>
+
+ function sitoInformation(){
+
 //my information Object goes here.
-const personalInformation = {
-"fullName": "Ogundare Israel Taiwo",
-"ID": "HNG-03186",
-"codeLanguage": "Javascript"
+
+  const personalInformation = {
+   fullName: "Ogundare Israel Taiwo",
+   ID: "HNG-03186",
+   programmeLang: "Javascript",
+   fullHistory: function() {
+    return `Hello world, this is ${this.fullName} with HNGi7 ID: ${this.ID} using ${this.programmeLang} for Stage 2 Task`;
+   }
+
 }
 
-//printing my information
-document.write(`Hello world, this is ${personalInformation.fullName} with HNGi7 ID: ${personalInformation.ID} using ${personalInformation.codeLanguage} for Stage 2 Task`);
+ document.write(personalInformation.fullHistory());
+
 
 }
 
-//calling my sitoInformation function
+//calling my sitoInformation function for a print
+
 sitoInformation();
+
+
+
+
+</script>
